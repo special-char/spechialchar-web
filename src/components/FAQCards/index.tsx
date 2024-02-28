baf440f4f1a35eb84af1c46676d4fe72dc502abc
Kreenashah36 <125737323+Kreenashah36@users.noreply.github.com>
2024-02-28 17:47:15 +0530

    FAQ changes done

--- src/components/FAQCards/index.tsx
+++ src/components/FAQCards/index.tsx
@@ -4,6 +4,7 @@ import { FAQCardType } from "@/utils/types";
 import { FAQCard } from "@/lib/constData";
 import Image from "next/image";
 import { Disclosure } from "@headlessui/react";
+import Button from "../Button";
 
 type Props = {};
 
@@ -13,18 +14,15 @@ const FAQ = ({ data }: FAQCardType) => {
     const togglePanel = (index: number) => {
         if (openIndexes.includes(index)) {
 
-            setOpenIndexes(openIndexes.filter((i) => i !== index));
+            setOpenIndexes(openIndexes.filter((i) => i == index));
         } else {
 
             setOpenIndexes([index]);
         }
     };
-
-
-
     return (
         <div className="px-container py-5 justify-center items-center font-sans leading-relaxed bg-[#f2aa4c]">
-            <h3 className="  py-10  h-full flex  flex-wrap flex-col items-left justify-left md:text-6xl font-bold bg-[#f2aa4c] text-2xl">How we can help you</h3>
+            <h3 className="  py-10  h-full flex  flex-wrap flex-col items-left justify-left md:text-4xl font-bold bg-[#f2aa4c] text-2xl">How we can help you</h3>
             {FAQCard.cards?.map((item, index) => {
                 const isOpen = openIndexes.includes(index);
                 return (
@@ -33,29 +31,41 @@ const FAQ = ({ data }: FAQCardType) => {
                             <>
                                 <Disclosure.Button
                                     key={item.id}
-                                    className="font-medium border-2 border-slate-200  flex w-full justify-between bg-[#f2aa4c]  px-3 py-4   cursor-pointer max-[450px]:w-full"
+                                    className="font-bold text-2xl border-t-2 border-black  flex w-full justify-between bg-[#f2aa4c]  px-3 py-4   cursor-pointer max-[450px]:w-full"
                                     onClick={() => togglePanel(index)}
                                 >
-                                    {item.question}
-
                                     <Image
                                         alt=""
-                                        src="/icons/accord_arrow_up.svg"
-                                        width={15}
-                                        height={15}
-                                        className={`${isOpen ? "" : "rotate-180 transform duration-300"} h-5 w-5 text-purple-500`}
+                                        src={item.url}
+                                        width={40}
+                                        height={40}
                                     />
+                                    <div className="py-3">{item.question}</div>
+                                    <div className="py-4">
+                                        <Image
+                                            alt=""
+                                            src="/icons/accord_arrow_up.svg"
+                                            width={15}
+                                            height={15}
+                                            className={`${isOpen ? "" : "rotate-180 transform duration-300"} h-5 w-5 text-purple-500`}
+
+                                        /></div>
                                 </Disclosure.Button>
                                 {isOpen && (
-                                    <Disclosure.Panel className="px-4 pb-4 mb-2 pt-4 text-sm  border-2 border-rose-300 bg-white  transition-all">
-                                        {item.answer}
+                                    <Disclosure.Panel className="px-4 md:px-10 mb-4 text-lg transition-all">
+
+                                        <div className="text-center"> {item.answer}</div>
                                     </Disclosure.Panel>
+
                                 )}
+
                             </>
                         )}
                     </Disclosure>
                 );
             })}
+            <hr className="border-b border-black" />
+            <Button className='md:w-72 w-60 my-14 font-bold'>Discuss my project</Button>
         </div>
     );
 };
