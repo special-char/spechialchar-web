f162b76cb07a43fdb327c2c491c7fde0a3030b45
suhani <suhanimodi06@gmail.com>
2024-02-29 15:58:29 +0530

    added careersbanner

---
+++ src/app/Careers/page.tsx
@@ -0,0 +1,18 @@
+
+import CareersBanner from '@/components/CareersBanner'
+import React from 'react'
+
+interface Props {
+    
+}
+
+const  Careers = (props: Props) => {
+    return (
+        <>
+        <CareersBanner/>
+       
+        </>
+    )
+}
+
+export default  Careers 
