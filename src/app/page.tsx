6f30c508b97d1ff2aa1d8a5a4d0031a94d48dc14
Harsh Baraiya <baraiyaharsh319@gmail.com>
2024-02-29 10:46:26 +0530

    Data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,17 @@ import Button from "@/components/Button";
 import HomeBanner from "@/components/HomeBanner";
 import Card from "@/containers/Card";
 import Icon from "@/public/icons/xmark.svg";
-import Contact from '../components/Contact/Contact'
-import Blog from "@/components/Blog/Blog";
+
+
+
 
 export default function Home() {
 	return (
 		<>
-
-			<HomeBanner />
+			
+			<HomeBanner />	
 			<Card />
+			
 			
 		</>
 	);
