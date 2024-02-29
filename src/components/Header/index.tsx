e15288e9fa0867d9747e06644c6ab8a4af08d80d
keval-parmar-030902 <kevalparmar030902@gmail.com>
2024-02-29 11:12:21 +0530

    header change

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -28,11 +28,10 @@ const Header = (props: Props) => {
   const [isOpen1, setIsOpen1] = useState(false);
   const [isOpen2, setIsOpen2] = useState(false);
 
-  const [isHovered, setIsHovered] = useState(false)
+  const [isHovered, setIsHovered] = useState(false);
   return (
-
     <header
-      className="h-20 z-10 fixed w-full bg-black flex gap-14 "
+      className="h-20 z-10 fixed w-full bg-black flex justify-around transition-transform duration-300"
       style={{
         backgroundColor:
           scrollPosition > 100 ? "rgba(0, 0, 0, 1)" : "rgba(0, 0, 0, 0)",
@@ -73,50 +72,53 @@ const Header = (props: Props) => {
             </nav>
       </div> */}
       <div className="py-4 text-slate-100 flex ">
-      <div className="relative">
-  <div
-    className="bg-inherit text-white px-4 py-2 flex items-center lg:gap-32 rounded cursor-pointer"
-    onClick={() => setIsOpen((prev) => !prev)}
-  >
-    <div>Services</div>
-    <div className="mb-1">
-      <Image
-        src="/icons/arrowdown.svg"
-        alt=""
-        fill
-        className={`${
-          !isOpen ? "" : "rotate-180 duration-300"
-        } h-3 w-3`}
-      ></Image>
-    </div>
-  </div>
-  {isOpen && (
-    <div className="bg-inherit ">
-      <a href="#" className="block py-1">
-        Mobile
-      </a>
-      <a href="#" className="block py-1">
-        Web
-      </a>
-      <a href="#" className="block py-1">
-        Saas
-      </a>
-      <a href="#" className="block py-1">
-        UI/Ux Design
-      </a>
-      <a href="#" className="block py-1">
-        Back-End
-      </a>
-    </div>
-  )}
-</div>
+        <div className="relative">
+          <div
+            className="bg-inherit text-white px-6 py-2 mx-auto flex justify-center gap-32 rounded cursor-pointer"
+            onClick={() => setIsOpen((prev) => !prev)}
+          >
+            <div className="mt-1">Services</div>
+            <div className="mb-1">
+              <Image
+                src="/icons/arrowdown.svg"
+                alt=""
+                fill
+                className={`${
+                  !isOpen ? "" : "rotate-180 duration-300"
+                } h-1 w-1`}
+              ></Image>
+            </div>
+          </div>
+          {isOpen && (
+            <div className="bg-black w-full px-6 py-2" style={{
+              backgroundColor:
+                scrollPosition > 100 ? "rgba(0, 0, 0, 1)" : "rgba(0, 0, 0, 0)",
+            }}>
+              <a href="#" className="block py-1">
+                Mobile
+              </a>
+              <a href="#" className="block py-1">
+                Web
+              </a>
+              <a href="#" className="block py-1">
+                Saas
+              </a>
+              <a href="#" className="block py-1">
+                UI/Ux Design
+              </a>
+              <a href="#" className="block py-1">
+                Back-End
+              </a>
+            </div>
+          )}
+        </div>
 
         <div className="relative">
           <div
-            className="bg-inherit text-white px-4 py-2 flex gap-32 rounded cursor-pointer"
+            className="bg-inherit text-white px-6 py-2 flex gap-32 rounded cursor-pointer"
             onClick={() => setIsOpen1((prev) => !prev)}
           >
-            <div>Industries</div>
+            <div className="mt-1">Industries</div>
             <div>
               <Image
                 src="/icons/arrowdown.svg"
@@ -129,7 +131,10 @@ const Header = (props: Props) => {
             </div>
           </div>
           {isOpen1 && (
-            <div className="bg-inherit absolute">
+            <div className="bg-black absolute w-full px-6 py-2" style={{
+              backgroundColor:
+                scrollPosition > 100 ? "rgba(0, 0, 0, 1)" : "rgba(0, 0, 0, 0)",
+            }}>
               <a href="#" className="block py-1">
                 EdTech & e-learning
               </a>
@@ -142,10 +147,10 @@ const Header = (props: Props) => {
 
         <div className="relative">
           <div
-            className="bg-inherit text-white px-4 py-2 flex gap-32 rounded cursor-pointer"
+            className="bg-inherit text-white px-6 py-2  flex gap-32 rounded cursor-pointer"
             onClick={() => setIsOpen2((prev) => !prev)}
           >
-            <div>Compnany</div>
+            <div className="mt-1">Compnany</div>
             <div>
               <Image
                 src="/icons/arrowdown.svg"
@@ -158,7 +163,10 @@ const Header = (props: Props) => {
             </div>
           </div>
           {isOpen2 && (
-            <div className="bg-inherit absolute">
+            <div className="bg-black w-full absolute px-6 py-2" style={{
+              backgroundColor:
+                scrollPosition > 100 ? "rgba(0, 0, 0, 1)" : "rgba(0, 0, 0, 0)",
+            }}>
               <a href="#" className="block py-1">
                 About
               </a>
@@ -171,16 +179,15 @@ const Header = (props: Props) => {
             </div>
           )}
         </div>
-        
+      <div className="py-4 -mt-1 text-slate-100 w-12">
+        <Link href={""}>Thinking</Link>
       </div>
-      <div className="py-4 text-slate-100">
-        <Link href={""}>Our Thinking</Link>
       </div>
-      <div className="py-4 text-slate-100">
-        <Link href={""}>Case Studies</Link>
+      <div className="py-4 mt-3 text-slate-100 w-12">
+        <Link href={""}>Projects</Link>
       </div>
-      <div className="py-4 text-slate-100">
-        <Button>Contact</Button>
+      <div className="py-4 mt-1 text-slate-100 w-12">
+        <Button className="p-7">Contact</Button>
       </div>
     </header>
   );
