dd4f9e4225bb8d03709f44c037d802053ad05660
keval-parmar-030902 <kevalparmar030902@gmail.com>
2024-02-29 17:23:33 +0530

    color change in rest of file

--- src/components/CareersBanner/index.tsx
+++ src/components/CareersBanner/index.tsx
@@ -13,9 +13,9 @@ const CareersBanner = () => {
         }}
       >
        
-            <h1 className="text-[#f2aa4c] sm:text-5xl md:text-7xl  lg:text-8xl font-extrabold text-center pt-40 md:pt-72  ">
+            <h1 className="text-orange sm:text-5xl md:text-7xl  lg:text-8xl font-extrabold text-center pt-40 md:pt-72  ">
             Build things you’re <br />
-            <span className="text-[#f2aa4c] pl-0 md:pl-28  lg:pl-16">proud of</span>
+            <span className="text-orange pl-0 md:pl-28  lg:pl-16">proud of</span>
             <div className="text-white text-lg md:text-2xl mt-4 md:mt-12 lg:mt-14 md:pl-6  text-center ">
               Join a crew as motivated as you. Make cool stuff for global brands.
               <br />
