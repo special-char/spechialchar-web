c3179fe38aa1f9f00e3122664c386ed4b281cdd3
Kreenashah36 <125737323+Kreenashah36@users.noreply.github.com>
2024-03-01 16:11:29 +0530

    Header done hamburger added

--- src/components/CareersBanner2/index.tsx
+++ src/components/CareersBanner2/index.tsx
@@ -1,18 +1,18 @@
 
 import React from "react";
 import Image from "next/image";
-import Banner2 from "@/public/images/CareersBanner2.webp";
+
 
 const CareersBanner2 = () => {
   return (
-    <div className="relative    md:mb-14 mb-5 ">
+    <div className="relative md:mb-14 mb-5 ">
       <Image
-        src={Banner2}
+        src="/images/flexibility.webp"
         alt="image"
-            height={200}
-            width={200}
+        height={700}
+        width={700}
         className="object-fit  w-full"
-       
+
       />
     </div>
   );
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,21 +1,25 @@
-"use client";
-import React, { useState ,useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
 import NavbarLogo from '@/public/NavbarLogo.png';
-import { TiArrowSortedDown } from "react-icons/ti";
-import Link from 'next/link';
+import { TiArrowSortedDown, TiThMenu } from "react-icons/ti";
+import Arrow from "@/public/icons/arrowdown.svg"
 import Button from '../Button';
 
 const Navbar = () => {
-  
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [activeDropdown, setActiveDropdown] = useState(null);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const dropdownRef = useRef(null);
 
-  const openSubDropdown = (subItem:any) => {
-    // Set the active dropdown to the subItem and open the dropdown
+  const openSubDropdown = (subItem: any) => {
     setActiveDropdown(subItem);
     setDropdownOpen(true);
   };
-
-  const [scrollPosition, setScrollPosition] = useState(0);
+  const openDropdown = (item) => {
+    setActiveDropdown(item);
+    setDropdownOpen(true);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -29,64 +33,32 @@ const Navbar = () => {
     };
   }, []);
 
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [activeDropdown, setActiveDropdown] = useState(null);
-
-  const openDropdown = (item) => {
-    setActiveDropdown(item);
-    setDropdownOpen(true);
-  };
 
   const closeDropdown = () => {
     setActiveDropdown(null);
     setDropdownOpen(false);
-  };
+  };
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
 
   return (
     <nav className="h-20 w-full fixed z-10 bg-black text-white p-4 2xl:px-64" style={{
-        backgroundColor:
-          scrollPosition > 100 ? "rgba(0, 0, 0, 1)" : "rgba(0, 0, 0, 0)",
-      }} onMouseLeave={closeDropdown} >
-      <div className="flex justify-between" >
+      backgroundColor:
+        scrollPosition > 100 ? "rgba(0, 0, 0, 1)" : "rgba(0, 0, 0, 0)",
+    }} onMouseLeave={closeDropdown}>
+      <div className="flex justify-between ">
         <div>
           <Image src={NavbarLogo} alt="Navbar" />
         </div>
-        <div>
-          <ul className="flex gap-x-6 2xl:pt-2 ">
-          <li onMouseEnter={() =>   openDropdown('Solution')  } className='flex cursor-pointer hover:text-yellow'>Solution  <span className='pt-1'><TiArrowSortedDown /></span></li>
-            <li onMouseEnter={() =>   openDropdown('Service') } className='flex cursor-pointer hover:text-yellow'>Service   <span className='pt-1'><TiArrowSortedDown /></span></li>
-            <li onMouseEnter={() =>   openDropdown('Industry')}  className='flex cursor-pointer hover:text-yellow'>Industry   <span className='pt-1'><TiArrowSortedDown /></span></li>
-            <li onMouseEnter={() =>   openDropdown('Company') } className='flex cursor-pointer hover:text-yellow'>Company   <span className='pt-1'><TiArrowSortedDown /></span></li>
-            <li className='hover:text-yellow '>Our Thinking</li>
-            <li className='hover:text-yellow'>Case Studio</li>
-          </ul>
+        <div className="lg:hidden">
+          <TiThMenu className="text-white cursor-pointer" size={30} onClick={toggleMobileMenu} />
         </div>
-        <div>
-          <Button className="bg-white  p-2">Contact Us</Button>
-        </div>
-      </div>
-      {dropdownOpen && (
-        <div className="absolute top-16 right-0 w-full bg-inherit  p-4 z-10">
-         <ul>
-      {activeDropdown === 'Solution' && (
-        <ul onClick={() => setDropdownOpen(false)} className='relative left-[30.3rem] gap-y-9 cursor-pointer'>
-          <li onClick={() => openSubDropdown('Data')}>Data</li>
-          {activeDropdown === 'Data' && (
-            <ul className='relative left-[630px] cursor-pointer'>
-              <li>Item 1</li>
-              <li>Item 2</li>
-              <li>Item 3</li>
-            </ul>
-          )}
-          <li>Artificial Intelligence</li>
-          <li>Automation</li>
-          <li>Blockchain</li>
-          <li>Extended Reality</li>
-          <li>Internet of Things</li>
-        </ul>
-      )}
-            {activeDropdown === 'Service' && (
-              <ul onClick={() => setDropdownOpen(false)} className='relative left-[36.6rem] cursor-pointer'>
+        <div className="hidden lg:flex justify-center gap-x-6 2xl:pt-2  " >
+          <li onMouseEnter={() => openDropdown('Service')} className='flex cursor-pointer gap-2'><div className='hover:text-yellow'>Service</div>    <span className={` ${activeDropdown === 'Service' && dropdownOpen ? 'rotate-180' : ''}`}><Arrow /></span>  {activeDropdown === 'Service' && (
+            <div className="absolute top-16  bg-inherit  z-10">
+              <ul onClick={() => setDropdownOpen(false)} className='relative  cursor-pointer'>
                 <li>Mobile</li>
                 <li>Web</li>
                 <li>Sass</li>
@@ -98,29 +70,93 @@ const Navbar = () => {
                 <li>Ui/Ux Design</li>
                 <li>Support&Maintance</li>
               </ul>
-            )}
-            {activeDropdown === 'Industry' && (
-              <ul onClick={() => setDropdownOpen(false)}  className='relative left-[42.8rem] cursor-pointer'>
-                <li>HealtCare&BioTech</li>
-                <li>Logistic&Transporention</li>
-                <li>ManuFacturing&Supply Chain</li>
-                <li>Retail</li>
-                <li>Edtech & Elearning</li>
-                <li>FinTech</li>
-                <li>Ecommerce</li>
-              </ul>
-            )}
-            {activeDropdown === 'Company' && (
-              <ul onClick={() => setDropdownOpen(false)}  className='relative left-[48.95rem] cursor-pointer'>
-                <li>About</li>
-                <li>Careers</li>
-                <li>How We are Work</li>
-              </ul>
-            )}
-          </ul>
+            </div>
+          )}</li>
+          <li onMouseEnter={() => openDropdown('Industry')} className='flex cursor-pointer gap-2'><div className='hover:text-yellow'>Industry</div>  <span className={` ${activeDropdown === 'Industry' && dropdownOpen ? 'rotate-180' : ''}`}><Arrow /></span>
+            <div className="absolute top-16  bg-inherit  z-10">
+              {activeDropdown === 'Industry' && (
+                <ul onClick={() => setDropdownOpen(false)} className='relative  cursor-pointer'>
+                  <li>HealtCare&BioTech</li>
+                  <li>Logistic&Transporention</li>
+                  <li>ManuFacturing&Supply Chain</li>
+                  <li>Retail</li>
+                  <li>Edtech & Elearning</li>
+                  <li>FinTech</li>
+                  <li>Ecommerce</li>
+                </ul>
+              )}
+            </div>
+          </li>
+          <li onMouseEnter={() => openDropdown('Company')} className='flex cursor-pointer gap-2'><div className='hover:text-yellow'>Company</div>    <span className={` ${activeDropdown === 'Company' && dropdownOpen ? 'rotate-180' : ''}`}><Arrow /></span>
+            <div className="absolute top-16  bg-inherit  z-10">
+              {activeDropdown === 'Company' && (
+                <ul onClick={() => setDropdownOpen(false)} className='relative  cursor-pointer'>
+                  <li>About</li>
+                  <li>Careers</li>
+                  <li>How We are Work</li>
+                </ul>
+              )}
+            </div>
+          </li>
+          <li className='hover:text-yellow list-none'>Our Thinking</li>
+          <li className='hover:text-yellow list-none'>Case Studio</li>
+
         </div>
-      )}
-    </nav>
+
+        <div className='hidden lg:block'>
+          <Button className="bg-slate-600 p-2">Contact Us</Button>
+        </div>
+      </div>
+      {/* Mobile Menu */}
+      {
+        mobileMenuOpen && (
+          <div className="lg:hidden fixed top-0 left-0 w-full h-full bg-black bg-opacity-90 flex  pl-10 pt-14" >
+            <div className="text-white text-2xl flex flex-col gap-y-4" >
+              <span onClick={toggleMobileMenu} className="absolute top-4 right-4 cursor-pointer text-3xl">&times;</span>
+              <div className="flex flex-col gap-y-2">
+
+                <li onMouseEnter={() => openDropdown('Service')} className='grid grid-cols-2 cursor-pointer  gap-2'><div className='hover:text-yellow' >Service </div><div className={`pt-1  ${activeDropdown === 'Service' && dropdownOpen ? 'rotate-180' : ''} `}><Arrow /></div>  {activeDropdown === 'Service' && (
+                  <div className="relative   bg-inherit  z-10">
+                    <ul onClick={() => setDropdownOpen(false)} className='  cursor-pointer'>
+                      <li>Mobile</li>
+                      <li>Web</li>
+                      <li>Sass</li>
+                      <li>Back-end</li>
+                      <li>Ui/Ux Design</li>
+                    </ul>
+                  </div>
+                )}</li>
+                <li onMouseEnter={() => openDropdown('Industry')} className='grid grid-cols-2 cursor-pointer gap-2'><div className='hover:text-yellow'>Industry</div>  <span className={` pt-1  ${activeDropdown === 'Industry' && dropdownOpen ? ' rotate-180' : ''}`}><Arrow /></span>
+                  <div className="relative  bg-inherit  z-10">
+                    {activeDropdown === 'Industry' && (
+                      <ul onClick={() => setDropdownOpen(false)} className='  cursor-pointer'>
+                        <li>Edtech & Elearning</li>
+                        <li>Ecommerce</li>
+                      </ul>
+                    )}
+                  </div>
+                </li>
+                <li onMouseEnter={() => openDropdown('Company')} className='grid grid-cols-2 cursor-pointer gap-2'><div className='hover:text-yellow'>Company</div>    <span className={`pt-1  ${activeDropdown === 'Company' && dropdownOpen ? ' rotate-180' : ''}`}><Arrow /></span>
+                  <div className="relative   bg-inherit  z-10">
+                    {activeDropdown === 'Company' && (
+                      <ul onClick={() => setDropdownOpen(false)} className='  cursor-pointer'>
+                        <li>About</li>
+                        <li>Careers</li>
+                        <li>How We are Work</li>
+                      </ul>
+                    )}
+                  </div>
+                </li>
+                <li className='hover:text-yellow list-none'>Our Thinking</li>
+                <li className='hover:text-yellow list-none'>Case Studio</li>
+
+              </div>
+              <Button className="bg-slate-600 p-2 mr-5">Contact Us</Button>
+            </div>
+          </div>
+        )
+      }
+    </nav >
   );
 };
 
