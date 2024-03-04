92586ca439d342b539f52e2ee816993aabf54e14
suhani <suhanimodi06@gmail.com>
2024-03-04 16:16:19 +0530

    UI banner added

---
+++ src/app/UI/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Banner from "@/components/UIBanner"
+interface Props {
+    
+}
+
+const UI = (props: Props) => {
+    return (
+       <>
+       <Banner/>
+       </>
+    )
+}
+
+export default UI
