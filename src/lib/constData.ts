c197f796445fc578d1e1000bad5f93766ef83d98
suhani <suhanimodi06@gmail.com>
2024-03-09 19:13:47 +0530

    added latest project and thinking

--- src/lib/constData.ts
+++ src/lib/constData.ts
@@ -9,3 +9,181 @@ export const footerdata = {
     privacy: "Privacy Policy",
     copyright: "@2024 Itrex",
   };
+
+  export const Cards = {
+    card: [
+    
+      {
+        id: 1,
+        url: "/images/project1.webp",
+        title: "AI-based enterprise software for No.1 retailer",
+        description:
+          "Self-service analytics enabled by AI algorithms empowers 3 million users with instant actionable insights",
+      },
+      {
+        id: 2,
+        url: "/images/project2.webp",
+        title: "Organ-on-a-chip platform for drug development",
+        description:
+          "Next-gen biotech platform relying on organ-chips enables faster, safer, and cheaper drug development for human use",
+      },
+      {
+        id: 3,
+        url: "/images/project3.webp",
+        title: "Predictive analytics for cancer clinicians",
+        description:
+          "ML-based analytics improves cancer prognostic accuracy and treatment recommendations. ",
+      },
+      {
+        id: 4,
+        url: "/images/project4.webp",
+        title: "Cloud infrastructure for social media giant",
+        description:
+          "Advanced cloud infrastructure connects data centers and on-premises systems to the cloud, driving agility in software development.",
+      },
+      {
+        id: 5,
+        url: "/images/project5.webp",
+        title: "Mobile app for Dollar Shave Club",
+        description:
+          "An Android app with no-fuss shopping features and flawless navigation helps the Dollar Shave Club skyrocket sales",
+      },
+      {
+        id: 6,
+        url: "/images/project6.webp",
+        title: "Next-level cybersecurity for enterprise risk management ",
+        description:
+          "A biometrics-based system provides bulletproof protection, eliminating credential deception and the need for passwords or PINs.",
+      },
+      {
+        id: 7,
+        url: "/images/project7.webp",
+        title: "AI-driven logistics platform",
+        description:
+          "An IoT platform offers extended shipment management options to 25,000 active users.",
+      },
+      {
+        id: 8,
+        url: "/images/project8.webp",
+        title: "Ecommerce solution for developer of virtual instruments ",
+        description:
+          "A tailored ecommerce platform has enabled a developer of software musical instruments to reach out to wider audiences.",
+      },
+      {
+        id: 9,
+        url: "/images/project9.webp",
+        title: "AI-powered autonomous checkout",
+        description:
+          " An ML-driven solution allows retailers to make their stores checkout-free without the need to redesign them.",
+      },
+      {
+        id: 10,
+        url: "/images/project10.webp",
+        title: "Cloud solution for US third-party benefits administrators",
+        description:
+          " Migration of a legacy system to microservices enables clients to easily configure and manage employee benefits.",
+      },
+    
+    ],
+  };
+
+
+  export const Card1 = {
+    cards: [
+      {
+        id: 1,
+        url: "/images/thinking1.webp",
+        title:
+          "Generative AI in business: top 5 use cases every company should consider [with a free eBook]",
+        description:
+          "Looking to make generative AI part of your company’s tech stack? Discover these promising generative AI applications in business to get inspired!",
+        height: 150,
+        width: 150,
+      },
+      {
+        id: 2,
+        url: "/images/thinking2.webp",
+        title:
+          "How to implement AI in your organization: the definitive guide [with a free eBook inside]",
+        description:
+          "How to implement AI in your business and achieve a substantial return on your artificial intelligence  investments? Our AI eBook will help you with that!",
+        height: 200,
+        width: 300,
+      },
+      {
+        id: 3,
+        url: "/images/thinking3.webp",
+        title:
+          "IPA vs. RPA vs. BPA: what is the right automation option for your company?",
+        description:
+          "Could you tell the difference between BPA, RPA, and IPA without running a few Google searches? If not, don’t bother surfing — we’ve done the job for you!",
+        height: 200,
+        width: 300,
+      },
+      {
+        id: 4,
+        url: "/images/thinking4.webp",
+        title: "Metaverse for business leaders: complete guide",
+        description:
+          "From immersive gaming environments to full-blown digital auctions where NFTs are traded, metaverse can be a multitude of things. And the reasons to start using metaverse for business are as diverse as the technology’s definition.",
+        height: 200,
+        width: 300,
+      },
+      {
+        id: 5,
+        url: "/images/thinking5.webp",
+        title: "ITRex launches two more initiatives to help Ukrainians",
+        description:
+          "Learn how ITRex continues to provide much-needed assistance to Ukranians living in crisis since Russia began its invasion.",
+        height: 200,
+        width: 300,
+      },
+      {
+        id: 6,
+        url: "/images/thinking6.webp",
+        title: "5 reasons to adopt IoT in sports and fitnes",
+        description:
+          "Discover the exciting applications of IoT in the sports sector and learn how to implement such solutions. Also, check out a project from our portfolio to see how IoT and AI can revolutionize our at-home training",
+        height: 200,
+        width: 300,
+      },
+  
+      {
+        id: 7,
+        url: "/images/thinking7.webp",
+        title: "How much does AI cost in 2024? Well, it depends",
+        description:
+          "Learn about the key factors influencing AI development costs and how you can spend less and see ROI faster.",
+        height: 200,
+        width: 300,
+      },
+      {
+        id: 8,
+        url: "/images/thinking8.webp",
+        title:
+          "Blockchain use cases, advantages, issues, and implementation tips",
+        description:
+          "Discover blockchain use cases and top benefits, and check out our tips on how to proceed with blockchain implementation.",
+        height: 200,
+        width: 300,
+      },
+      {
+        id: 9,
+        url: "/images/thinking9.webp",
+        title: "AI in the Supply Chain: Use Cases &amp; Implementation Roadmap",
+        description:
+          "Explore the ways AI transforms the global supply chain and learn how to implement the digital supply chain with no fuss.",
+        height: 200,
+        width: 300,
+      },
+      {
+        id: 10,
+        url: "/images/thinking10.webp",
+        title: "How to get started with enterprise automation",
+        description:
+          " Check out our new article to discover what ready-made and custom enterprise workflow automation tools have in store and which option is best for you.",
+        height: 200,
+        width: 300,
+      },
+    ],
+  };
