9a12220ca0a3d2b7e98535380089bb6c768d4041
keval-parmar-030902 <kevalparmar030902@gmail.com>
2024-03-15 11:04:03 +0530

    about page banner called

--- src/app/company/about/page.tsx
+++ src/app/company/about/page.tsx
@@ -1,7 +1,17 @@
 import AboutBanner from "@/components/AboutBanner";
+import AboutBanner2 from "@/components/AboutBanner2";
 import AboutCount from "@/components/AboutCount";
+import ImageBanner from "@/components/ImageBanner";
+import LatestThinking from "@/components/LatestThinking";
 import MobilePage from "@/components/MobilePage";
-import { aboutBannerdata } from "@/lib/constData";
+import OurClients from "@/container/OurClients";
+import {
+  aboutBanner2,
+  aboutBannerdata,
+  imageBanner,
+  imageBanner1,
+  latestThinkingData,
+} from "@/lib/constData";
 import React from "react";
 
 type Props = {};
@@ -12,6 +22,10 @@ const AboutPage = (props: Props) => {
       <AboutBanner data={aboutBannerdata} />
       {/* <MobilePage /> */}
       <AboutCount />
+      <OurClients />
+      <AboutBanner2 data={aboutBanner2} />
+      <ImageBanner data={imageBanner} />
+      <LatestThinking data={latestThinkingData} />
     </>
   );
 };
