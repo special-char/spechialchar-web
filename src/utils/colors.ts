4adebeffbebc1eb3c7ea44f24caf57d85935c8f9
Kreenashah36 <125737323+Kreenashah36@users.noreply.github.com>
2024-04-02 16:25:12 +0530

    header

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -14,10 +14,17 @@ const data: PageData[] = [
     color1: "text-yellow",
     color2: "text-background",
   },
+  {
+    page: "services",
+    bgColor: "",
+    color: "text-primary",
+    color1: "text-yellow",
+    color2: "text-background",
+  },
 ];
 
 const getColors = (pageName: string): PageData | undefined => {
   return data.find((x) => x.page === pageName);
 };
 
-export default getColors("home");
+export default getColors;
