2356c6da83999f59559bc35e03cae438d65efc26
keval-parmar-030902 <kevalparmar030902@gmail.com>
2024-04-11 14:51:15 +0530

    link changes done

--- src/container/CloudAccordian/index.tsx
+++ src/container/CloudAccordian/index.tsx
@@ -21,7 +21,7 @@ const CloudAccordian = ({ data }: CloudAccordiandataType) => {
         <CloudAccord data={cloudAccordiandata} />
       </div>
       <div>
-        <Link title="cloud-late-talk" href={"https://thespecialcharacter.graphy.com/"}>
+        <Link title="cloud-late-talk" href={"#Contact"}>
           <Button variant="default" size="sm" className=" py-7 text-base ">
             Lets Talk
           </Button>
