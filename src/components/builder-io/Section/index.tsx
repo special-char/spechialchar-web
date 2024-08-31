ccb43a149a82b50b0c9a6fdfc39cf1fb90b0430e
Aaryan Patel <aaryan_20061@ldrp.ac.in>
2024-08-31 16:56:16 +0530

    commit

--- src/components/builder-io/Section/index.tsx
+++ src/components/builder-io/Section/index.tsx
@@ -2,7 +2,7 @@ import { withChildren } from "@builder.io/react";
 import React, { PropsWithChildren } from "react";
 
 const Section = withChildren((props: PropsWithChildren) => {
-  return <section className="px-container py-10">{props.children}</section>;
+  return <section className="px-container py-12">{props.children}</section>;
 });
 
 export default Section;
