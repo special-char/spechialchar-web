7f1692dc76ff19b1c65d50c881a541336f739485
akshpatell <133746090+akshpatell@users.noreply.github.com>
2024-09-02 17:42:42 +0530

    Builderio (#266) (#267)
    * Merge branch 'development' of https://github.com/special-char/spechialchar-web into builderio/services_mobile
    
    * Merge branch 'development' of https://github.com/special-char/spechialchar-web into builderio/services_mobile
    
    * heading component updated
    
    * Merge branch 'builderio/services_mobile' into test

--- src/components/builder-io/BiHeading/index.tsx
+++ src/components/builder-io/BiHeading/index.tsx
@@ -1,18 +1,47 @@
 import { cn } from "@/lib/utils";
-import { withChildren } from "@builder.io/react";
-import React, { PropsWithChildren } from "react";
+import React from "react";
 
 type Props = {
-  level: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  level: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p";
   content?: string;
   className?: string;
-} & PropsWithChildren;
+  attributes?: any;
+  variantOfPTag?: "subtitle1" | "subtitle2" | "small" | "sm" | "large";
+  variantOfHeadingTag?:
+    | "text-heading1"
+    | "text-heading2"
+    | "text-heading3"
+    | "text-heading4"
+    | "text-heading5"
+    | "text-heading6"
+    | "text-subtitle1"
+    | "text-subtitle2"
+    | "text-subtitle3"
+    | "text-body1"
+    | "text-body2"
+    | "text-caption1"
+    | "text-caption2";
+};
 
-const BIHeading = ({ level = "h1", content, className }: Props) => {
+const BIHeading = ({
+  level = "h1",
+  content,
+  className,
+  variantOfPTag,
+  variantOfHeadingTag,
+  attributes,
+}: Props) => {
   const Comp = level;
   return (
-    <Comp className={cn("", { [`${className}`]: !!className })}>{content}</Comp>
+    <Comp
+      {...attributes}
+      className={cn("", `${variantOfPTag}`, `${variantOfHeadingTag}`, {
+        [`${className}`]: !!className,
+      })}
+    >
+      {content}
+    </Comp>
   );
 };
 
-export default withChildren(BIHeading);
+export default BIHeading;
