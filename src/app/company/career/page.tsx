896fa4db7561b812f4fefcea17ed214829181f0a
akshpatel <akshpatel08032003@gmail.com>
2024-09-05 10:27:58 +0530

    career page staring with builder.io

--- src/app/company/career/page.tsx
+++ src/app/company/career/page.tsx
@@ -1,59 +1,86 @@
+// import CareerBanner from "@/components/CareerBanner";
+// import CareerBanner2 from "@/components/CareerBanner2";
+// import CareerCV from "@/components/CareerCV";
+// import CareerCap from "@/components/CareerCap";
+// import CareerCity from "@/components/CareerCity";
+// import CareerMany from "@/components/CareerMany";
+// import CareerPage2 from "@/components/CareerPage2";
+// import CareerPage4 from "@/components/CareerPage4";
+// import CareerPage5 from "@/components/CareerPage5";
+// import CareerSection from "@/components/CareerSection";
+// import CareerSection2 from "@/components/CareerSection2";
+// import {
+//   career,
+//   career2,
+//   careerBanner2,
+//   careerbannerdata,
+//   careercap,
+//   careercity,
+//   careercv,
+//   careermany,
+//   careerpage2data,
+//   careerpage4data,
+//   careerpage5data,
+//   careerpage6data,
+//   careerpage7data,
+// } from "@/lib/constData";
+// import React from "react";
+// import { Metadata } from "next";
 
-import CareerBanner from "@/components/CareerBanner";
-import CareerBanner2 from "@/components/CareerBanner2";
-import CareerCV from "@/components/CareerCV";
-import CareerCap from "@/components/CareerCap";
-import CareerCity from "@/components/CareerCity";
-import CareerMany from "@/components/CareerMany";
-import CareerPage2 from "@/components/CareerPage2";
-import CareerPage4 from "@/components/CareerPage4";
-import CareerPage5 from "@/components/CareerPage5";
-import CareerSection from "@/components/CareerSection";
-import CareerSection2 from "@/components/CareerSection2";
-import {
-  career,
-  career2,
-  careerBanner2,
-  careerbannerdata,
-  careercap,
-  careercity,
-  careercv,
-  careermany,
-  careerpage2data,
-  careerpage4data,
-  careerpage5data,
-  careerpage6data,
-  careerpage7data,
-} from "@/lib/constData";
-import React from "react";
-import { Metadata } from "next";
+// export const metadata: Metadata = {
+//   title: "Career Oppertunity",
+//   description:
+//     "Learn practically with us and get ahead of the competition with our industry-relevant IT Training. Our expertise - Digital Marketing | Full Stack Development",
+// };
 
-export const metadata: Metadata = {
-  title: "Career Oppertunity",
-  description:
-    "Learn practically with us and get ahead of the competition with our industry-relevant IT Training. Our expertise - Digital Marketing | Full Stack Development",
-};
+// type Props = {};
 
-type Props = {};
+// const AboutPage = (props: Props) => {
+//   return (
+//     <>
+//       <CareerBanner data={careerbannerdata} />
+//       <CareerPage2 data={careerpage2data} />
+//       <CareerCap data={careercap} />
+//       <CareerPage4 data={careerpage4data} />
+//       <CareerPage5 data={careerpage5data} />
+//       <CareerPage2 data={careerpage6data} />
+//       <CareerPage4 data={careerpage7data} />
+//       <CareerSection data={career} />
+//       <CareerBanner2 data={careerBanner2} />
+//       <CareerSection2 data={career2} />
+//       <CareerCity data={careercity} />
+//       <CareerMany data={careermany} />
+//       <CareerCV data={careercv} />
+//     </>
+//   );
+// };
 
-const AboutPage = (props: Props) => {
+// export default AboutPage;
+
+import { RenderBuilderContent } from "@/components/builder";
+import { builder } from "@builder.io/sdk";
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
+
+interface PageProps {
+  params: {
+    page: string[];
+  };
+}
+
+export default async function Page(props: PageProps) {
+  const builderModelName = "page";
+
+  const content = await builder
+    .get(builderModelName, {
+      userAttributes: {
+        urlPath: "/company/career",
+      },
+    })
+    .toPromise();
+
   return (
     <>
-      <CareerBanner data={careerbannerdata} />
-      <CareerPage2 data={careerpage2data} />
-      <CareerCap data={careercap} />
-      <CareerPage4 data={careerpage4data} />
-      <CareerPage5 data={careerpage5data} />
-      <CareerPage2 data={careerpage6data} />
-      <CareerPage4 data={careerpage7data} />
-      <CareerSection data={career} />
-      <CareerBanner2 data={careerBanner2} />
-      <CareerSection2 data={career2} />
-      <CareerCity data={careercity} />
-      <CareerMany data={careermany} />
-      <CareerCV data={careercv} />
+      <RenderBuilderContent content={content} model={builderModelName} />
     </>
   );
-};
-
-export default AboutPage;
+}
