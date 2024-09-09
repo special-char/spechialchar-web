7bd8df40bf760573e053e80efa2e8d0d208cd5dc
HeenaPanchal <heenapanchal2921@gmail.com>
2024-09-09 09:59:13 +0530

    created how-we-work page into company

--- src/app/company/how-we-work/page.tsx
+++ src/app/company/how-we-work/page.tsx
@@ -1,29 +1,62 @@
-import HowBanner from '@/components/HowBanner'
-import HowModel from '@/components/HowModel'
-import Tab2 from '@/components/Tab/index2'
-import TableDemo from '@/components/Tabel'
-import { howWeWorktabdata, howbanner, howmodel } from '@/lib/constData'
-import React from 'react'
-import { Metadata } from "next";
+// import HowBanner from '@/components/HowBanner'
+// import HowModel from '@/components/HowModel'
+// import Tab2 from '@/components/Tab/index2'
+// import TableDemo from '@/components/Tabel'
+// import { howWeWorktabdata, howbanner, howmodel } from '@/lib/constData'
+// import React from 'react'
+// import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "How We Work",
-  description:
-    "Learn practically with us and get ahead of the competition with our industry-relevant IT Training. Our expertise - Digital Marketing | Full Stack Development",
-};
+// export const metadata: Metadata = {
+//   title: "How We Work",
+//   description:
+//     "Learn practically with us and get ahead of the competition with our industry-relevant IT Training. Our expertise - Digital Marketing | Full Stack Development",
+// };
 
-type Props = {}
+// type Props = {}
 
-const HowWeWork = (props: Props) => {
-  return (
-    <>
-      <HowBanner data={howbanner} />
-      <Tab2 data={howWeWorktabdata} />
-      <HowModel data={howmodel} />
-      <TableDemo />
+// const HowWeWork = (props: Props) => {
+//   return (
+//     <>
+//       <HowBanner data={howbanner} />
+//       <Tab2 data={howWeWorktabdata} />
+//       <HowModel data={howmodel} />
+//       <TableDemo />
 
-    </>
-  )
+//     </>
+//   )
+// }
+
+// export default HowWeWork
+
+import { RenderBuilderContent } from "@/components/builder";
+import { builder } from "@builder.io/sdk";
+// Builder Public API Key set in .env file
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
+
+interface PageProps {
+  params: {
+    page: string[];
+  };
 }
 
-export default HowWeWork
+export default async function Page(props: PageProps) {
+  const builderModelName = "page";
+
+  const content = await builder
+    // Get the page content from Builder with the specified options
+    .get(builderModelName, {
+      userAttributes: {
+        // Use the page path specified in the URL to fetch the content
+        urlPath: "/company/how-we-work",
+      },
+    })
+    // Convert the result to a promise
+    .toPromise();
+
+  return (
+    <>
+      {/* Render the Builder page */}
+      <RenderBuilderContent content={content} model={builderModelName} />
+    </>
+  );
+}
