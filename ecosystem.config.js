14ccc533165ae2473ef0f403bfea3d5360461bac
Yagnesh Modh <yagnesh.modh@gmail.com>
2024-09-17 04:38:39 +0000

    deploy setup

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+        apps: [
+                {
+                        name: "tsc_it_web",
+                        script: "yarn",
+                        args: "start",
+                },
+        ],
+};
