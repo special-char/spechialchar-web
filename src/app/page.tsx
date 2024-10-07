a05b2912f1da8fe264622176b911fc14836f25e6
aaryan8575 <aaryan_20061@ldrp.ac.in>
2024-10-07 18:04:57 +0530

    json ld home page test 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,7 +77,8 @@ const jsonLd = {
   url: "https://it.thespecialcharacter.com",
   description:
     "Next-gen enterprise software development company offering services in web, mobile, SaaS development, and UI/UX design.",
-  logo: "https://cdn.builder.io/api/v1/image/assets%2Fdbf92adf2ef043c7957411ad39146bdb%2F4b3d91a33b334a809b20929c601f847e",
+  image:
+    "https://cdn.builder.io/api/v1/image/assets%2Fdbf92adf2ef043c7957411ad39146bdb%2F4b3d91a33b334a809b20929c601f847e",
   publisher: {
     "@type": "Organization",
     name: "The Special Character",
@@ -85,18 +86,19 @@ const jsonLd = {
     logo: "https://cdn.builder.io/api/v1/image/assets%2Fdbf92adf2ef043c7957411ad39146bdb%2F4b3d91a33b334a809b20929c601f847e",
     contactPoint: {
       "@type": "ContactPoint",
-      addressLocality: "Ahemdabad",
-      postalCode: "382470",
-      streetAddress:
-        "B-604/605 Ganesh Glory 11 Jagarpur Road, SG Highway Gujarat 382470",
       email: "mailto:contact@thespecialcharacter.com",
       telephone: "+91 7600096432",
       contactType: "Customer Service",
+      address: {
+        "@type": "PostalAddress",
+        addressLocality: "Ahmedabad",
+        postalCode: "382470",
+        streetAddress:
+          "B-604/605 Ganesh Glory 11 Jagarpur Road, SG Highway Gujarat",
+      },
     },
-    sameAs: [
-      "https://www.linkedin.com/company/thespecialcharacter",
-      "https://www.instagram.com/thespecialcharacter",
-    ],
+
+    sameAs: ["https://www.linkedin.com/company/thespecialcharacter"],
   },
 };
 
@@ -118,7 +120,9 @@ export default async function Page(props: PageProps) {
     <>
       <script
         type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        dangerouslySetInnerHTML={{
+          __html: jsonLd ? JSON.stringify(jsonLd) : "",
+        }}
       />
       {/* Render the Builder page */}
       <RenderBuilderContent content={content} model={builderModelName} />
